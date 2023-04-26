6241b7fd46c044b853bab0115a7edeae458dff8e
Alex <alex@alinator2.fritz.box>
2023-04-26 16:46:44 +0200

    Fixes tflint error

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "topic_name" {
-  value = google_pubsub_topic.example_resource.name
+  description = "Name of created pubsub topic"
+  value       = google_pubsub_topic.example_resource.name
 }
